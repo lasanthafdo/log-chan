8983ebb66ad59c2d0477f7fbc0f977d9646ac151
samilakkd@gmail.com <samilakkd@gmail.com@9506b0a7-2315-58da-3d96-016a8765b8d7>
2012-08-23 15:07:15 +0000

    logging

--- LogChan/src/org/logchan/core/LogChanApp.java
+++ LogChan/src/org/logchan/core/LogChanApp.java
@@ -1,22 +1,22 @@
 package org.logchan.core;
 
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
-import org.logchan.rules.RuleManager;
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
 import org.logchan.ui.UserInterface;
 
 public class LogChanApp {
-
+	private static final Logger log = Logger.getLogger(LogChanApp.class);
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-        try {
+		PropertyConfigurator.configure("lib/log4j.properties");
+		log.debug("LogChan launched");
+        try {        	
             new UserInterface().setVisible(true);
         } catch (Exception ex) {
-            Logger.getLogger(LogReader.class.getName()).log(Level.SEVERE, null, ex);
+        	log.fatal("LogChan initialize error");
         }
 	}
 
