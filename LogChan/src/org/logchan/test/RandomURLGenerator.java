9a3d52154003202539dd02265fea7c4a95455b09
lasantha.fdo@gmail.com <lasantha.fdo@gmail.com@9506b0a7-2315-58da-3d96-016a8765b8d7>
2012-09-08 17:02:34 +0000

    Added a new trial version of Jess. Moved the RandomURLGenerator to test package

--- LogChan/src/org/logchan/test/RandomURLGenerator.java
+++ LogChan/src/org/logchan/test/RandomURLGenerator.java
@@ -1,4 +1,4 @@
-package org.logchan.util;
+package org.logchan.test;
 
 import java.io.BufferedWriter;
 import java.io.File;
@@ -11,20 +11,20 @@ import java.util.Random;
 import java.util.Set;
 
 public class RandomURLGenerator {
-	
-    private static final String lCase = "abcdefghijklmnopqrstuvwxyz";
-    private static final String uCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    private static final String sChar = "!@#$%^&*";
-    private static final String intChar = "0123456789";
-	
-	private Set<String> getRandomStrings(int stringCount){
+
+	private static final String lCase = "abcdefghijklmnopqrstuvwxyz";
+	private static final String uCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	private static final String sChar = "!@#$%^&*";
+	private static final String intChar = "0123456789";
+
+	private Set<String> getRandomStrings(int stringCount) {
 		Random randomLength = new Random();
 		Random randomCharacterType = new Random();
 		Set<String> stringSet = new HashSet<String>();
-		for(int i=0;i<stringCount;i++){
+		for (int i = 0; i < stringCount; i++) {
 			int stringLength = randomLength.nextInt(100);
 			StringBuilder string = new StringBuilder();
-			while(string.length() != stringLength){
+			while (string.length() != stringLength) {
 				int characterType = randomCharacterType.nextInt(4);
 				switch (characterType) {
 				case 0:
@@ -51,39 +51,40 @@ public class RandomURLGenerator {
 		}
 		return stringSet;
 	}
-	
-	public Set<URL> getURLSet(int urlCount){
+
+	public Set<URL> getURLSet(int urlCount) {
 		Set<String> stringSet = getRandomStrings(urlCount);
 		Set<URL> urlSet = new HashSet<URL>();
-		for(String string:stringSet){
+		for (String string : stringSet) {
 			try {
 				URL url = new URL("http", "example.com/", string + ".html");
 				urlSet.add(url);
 			} catch (MalformedURLException e) {
-				//TODO log
+				// TODO log
 			}
 		}
 		return urlSet;
 	}
-	
+
 	public static void main(String[] args) {
+		System.out.println("Starting generation of log file...");
 		RandomURLGenerator gen = new RandomURLGenerator();
 		Set<URL> set = gen.getURLSet(250);
 		String beforeUrl = "10.116.47.101 - - [15/Jun/2012:16:23:47 +0530] \"GET /testfiles/korra.tar.gz HTTP/1.1\" 200 6999656628 ";
-		String afterUrl  = " \"Mozilla/5.0 (Windows NT 5.1; rv:13.0) Gecko/20100101 Firefox/13.0\"";
-		File file = new File("F:/sampleLog.lg");
+		String afterUrl = " \"Mozilla/5.0 (Windows NT 5.1; rv:13.0) Gecko/20100101 Firefox/13.0\"";
+		File file = new File("generated_access_log.log");
 		try {
-			BufferedWriter writer = new BufferedWriter(new FileWriter(file,true));
-			for(URL url : set){
-				writer.write(beforeUrl + "\"" + url.toString() + "\"" + afterUrl);
+			BufferedWriter writer = new BufferedWriter(new FileWriter(file,
+					true));
+			for (URL url : set) {
+				writer.write(beforeUrl + "\"" + url.toString() + "\""
+						+ afterUrl);
 				writer.newLine();
 			}
 			writer.close();
 		} catch (IOException e) {
+			e.printStackTrace();
 		}
-		
-
-
-		
+		System.out.println("Done.");
 	}
 }
